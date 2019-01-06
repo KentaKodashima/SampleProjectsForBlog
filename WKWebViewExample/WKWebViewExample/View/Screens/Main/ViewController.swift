86162110b3dece9ef3d6e8f32fabe88668c87170
Kenta Kodashima <kentakodashima@gmail.com>
2019-01-05 18:25:46 -0800

    Started working on the WKWebView sample project.

---
+++ WKWebViewExample/WKWebViewExample/View/Screens/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebViewExample
+//
+//  Created by Kenta Kodashima on 2019-01-05.
+//  Copyright © 2019 Kenta Kodashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  
+}
+
